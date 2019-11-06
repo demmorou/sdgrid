de9c6ea109141bd2abd8cc3ff5cc99a14808be96
francisco <franciscojn49@gmail.com>
2019-11-06 16:50:44 -0300

    app realizando conecao socket io client

---
+++ mobile/SDGrid/src/config/colors.js
@@ -0,0 +1,13 @@
+const colors = {
+    primaryColor: '#eeeeee',
+    primaryDarkColor: '#bcbcbc',
+    secondaryColor: '#701719',
+    blue: '#1E90FF',
+    white: '#ffffff',
+    black: '#212121',
+    background: '#f0f0f0',
+    backgroundView: '#f0f0f0',
+    colorNegativeButton: '#595959'
+  };
+
+export default colors;
---
+++ mobile/SDGrid/src/config/styles.js
@@ -0,0 +1,47 @@
+import {StyleSheet, Platform, StatusBar} from 'react-native';
+import colors from './colors';
+
+const styles = StyleSheet.create({
+    viewInput: {
+        borderRadius: 3,
+        flexDirection: 'row', 
+        alignItems: 'center', 
+        justifyContent: "center", 
+        marginTop: 20,
+        padding: 3,
+        borderTopWidth: 0,
+        borderStartWidth: 0,
+        borderEndWidth: 0,
+        borderBottomWidth: 1,
+        borderBottomColor: colors.black,
+        backgroundColor: '#f0f0f0',
+        fontSize: 16
+    },
+    
+    input: {
+        width: '95%',
+        height: 49,
+        paddingStart: 13,
+        backgroundColor: '#f0f0f0',
+        marginBottom: 3,
+        fontSize: 18
+      },
+      
+    button: {
+        width: '80%',
+        height: 49,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 3,
+        backgroundColor: colors.blue,
+        marginTop: 20,
+    },
+
+    text: {
+        fontSize: 18,
+        color: colors.white,
+      },
+
+});
+
+export default styles;
